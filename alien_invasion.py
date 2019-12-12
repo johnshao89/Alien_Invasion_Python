84727591c90f349ff275f93f027d0fd47e83cd2b
johnshao89 <wjshao2010@gmail.com>
2019-12-11 22:33:12 -0500

    updated for bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -11,20 +12,23 @@ class AlienInvasion:
         pygame.init()
 
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect.width()
-        self.settings.screen_height = self.screen.get_rect.height()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        #self.settings.screen_width = self.screen.get_rect.width()
+        #self.settings.screen_height = self.screen.get_rect.height()
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
         # No longer needed self.bg_color = (230,230,230)
         self.ship = Ship(self)
 
+        self.bullets = pygame.sprite.Group()
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_event()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
 
@@ -38,13 +42,6 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
-
-    def _update_screen(self):
-        """A helper method work inside the class but not meant to be called by instance"""
-        self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
-        pygame.display.flip()
-
     def _check_keydown_events(self,event):    
         if event.key == pygame.K_RIGHT:
             self.ship.move_right = True
@@ -52,12 +49,31 @@ class AlienInvasion:
             self.ship.move_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.move_right = False
-        elif event.key ==pygame.K_LEFT:
-            self.ship.move_left = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.move_left = False        
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+    def _update_screen(self):
+        """A helper method work inside the class but not meant to be called by instance"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
+
+
+
+
 
 if __name__ == '__main__':
     #Make a game instance and run the game.
